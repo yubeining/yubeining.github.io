af339cb8da9da052494a58df2b6f978adc7e045d
yubeining <yubeining@users.noreply.github.com>
2025-07-01 16:48:03 +0000

    deploy: 6cd0a75d6d13f2484b931e3d8ea7241889a17ac7

---
+++ assets/_...path_.GpFW-bpC.js
@@ -0,0 +1,1 @@
+import{a,u as s,e as t}from"./chunks/vue-router.CE6eJGTm.js";import{f as n,H as r,Q as c,P as p}from"./framework.CgPPQfmH.js";const u=n({__name:"[...path]",setup(f){const o=s(),e=a();return r(()=>{e.path!=="/"&&e.path.endsWith("/")&&o.replace(e.path.slice(0,-1))}),(i,m)=>(p(),c("div"))}});typeof t=="function"&&t(u);export{u as default};
---
+++ assets/index.Ch_fz0Yr.js
@@ -0,0 +1,15 @@
+import{_ as p}from"./ValaxyMain.vue_vue_type_style_index_0_lang.DXFDIcRI.js";import{f,a as c,u as d}from"./chunks/vue-router.CE6eJGTm.js";import{O as h,a4 as t,u as y,P as b,V as o,S as v,A as $}from"./framework.CgPPQfmH.js";import"./app.2Hk-Pb-P.js";import"./chunks/dayjs.CCYrSalk.js";import"./chunks/vue-i18n.BoM8szIi.js";import"./chunks/pinia.BjFmakTL.js";import"./chunks/@vueuse/motion.Cp--YhDB.js";import"./chunks/nprogress.BahbDzmd.js";import"./YunComment.vue_vue_type_style_index_0_lang.DC8N30BB.js";import"./index.C5okkQwF.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang.PEAAi8W5.js";import"./post.CGlAV6UI.js";const w=f("/about",async e=>JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"pages/about/index.md","lastUpdated":1745542762000}'),{lazy:(e,s)=>e.name===s.name}),x={__name:"index",setup(e,{expose:s}){var i;const{data:r}=w(),u=d(),m=c(),a=Object.assign(m.meta.frontmatter||{},((i=r.value)==null?void 0:i.frontmatter)||{});return m.meta.frontmatter=a,u.currentRoute.value.data=r.value,$("valaxy:frontmatter",a),globalThis.$frontmatter=a,s({frontmatter:{}}),(n,Y)=>{const l=p;return b(),h(l,{frontmatter:y(a)},{"main-content-md":t(()=>[v(` ---
+title: 关于我
+---
+
+I am developing [Valaxy](https://github.com/YunYouJun/valaxy) - Next Generation Static Blog Framework.
+
+If you like it, you can sponsor me in [GitHub](https://github.com/sponsors/YunYouJun) or [www.yunyoujun.cn/sponsors/](https://www.yunyoujun.cn/sponsors/).
+
+---
+
+我正在开发 [Valaxy](https://github.com/YunYouJun/valaxy) - 下一代静态博客框架。
+
+如果你喜欢它，你可以在 [GitHub](https://github.com/sponsors/YunYouJun) 或 [www.yunyoujun.cn/sponsors/](https://www.yunyoujun.cn/sponsors/) 赞助我。
+
+Check out the [Valaxy | GitHub](https://github.com/YunYouJun/valaxy) for more details. `)]),"main-header":t(()=>[o(n.$slots,"main-header")]),"main-header-after":t(()=>[o(n.$slots,"main-header-after")]),"main-nav":t(()=>[o(n.$slots,"main-nav")]),"main-content-before":t(()=>[o(n.$slots,"main-content-before")]),"main-content":t(()=>[o(n.$slots,"main-content")]),"main-content-after":t(()=>[o(n.$slots,"main-content-after")]),"main-nav-before":t(()=>[o(n.$slots,"main-nav-before")]),"main-nav-after":t(()=>[o(n.$slots,"main-nav-after")]),comment:t(()=>[o(n.$slots,"comment")]),footer:t(()=>[o(n.$slots,"footer")]),aside:t(()=>[o(n.$slots,"aside")]),"aside-custom":t(()=>[o(n.$slots,"aside-custom")]),default:t(()=>[o(n.$slots,"default")]),_:3},8,["frontmatter"])}}};export{x as default,w as usePageData};
